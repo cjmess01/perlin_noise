01a15845726406845aff2fefc842f1dae3257c68
Caleb <cjmess01@gmail.com>
2026-02-04 21:22:56 -0600

    User input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    const MAX_WIDTH: usize = 1_000_000;
+    const DEFAULT: usize = 10;
+
+    let mut width = String::new();
+    io::stdin()
+        .read_line(&mut width)
+        .expect("Failed to read line");
+
+    // if weird value, send back 10
+    let width: usize = match width.trim().parse() {
+        Ok(num) if num <= MAX_WIDTH => num,
+        Ok(_) => {
+            println!("Number is too large, using default");
+            DEFAULT
+        }
+        Err(_) => {
+            println!("Expected a number, got invalid value");
+            DEFAULT
+        }
+    };
+
+    // let mut v: Vec<i32> = Vec::new();
+    //
+    // for number in 0..width {
+    //     v.push(number as i32);
+    // }
+    //
+    // for (i, &item) in v.iter().enumerate() {
+    //     println!("{i}:{item}");
+    // }
+
+    // let mut v: Vec<Vec<f32>> = Vec::new();
+    // for number in 0..width {
+    //     println!("{number}");
+    //     let sub_list = &mut v[number];
+    //     let elem = sub_list.get(0);
+    //     println!("{elem}");
+    // }
 }
