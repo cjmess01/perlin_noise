5d73f148eef3f8456b1f32f29ad35109c03e4f7b
cjmess01 <cjmess01@gmail.com>
2026-02-25 16:48:24 -0600

    added noise engine

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,64 @@
 use std::env;
 
 mod noise;
+use image::{ImageBuffer, Rgb};
 
 fn main() -> Result<(), String> {
     let args: Vec<String> = env::args().collect();
     let config = parse_arguments(args)?;
 
-    let val = noise::noise(0.0, 0.0);
+    println!("{} {} {}", config.width, config.height, config.frequency);
+
+    let mut max_val = 0.0;
+    let mut min_val = 0.0;
+    // This creates a png and fills it with the info
+    let mut img = ImageBuffer::new(config.width as u32, config.height as u32);
+    for (x, y, pixel) in img.enumerate_pixels_mut() {
+        let nx = (x as f64 / config.width as f64) * config.frequency;
+        let ny = (y as f64 / config.height as f64) * config.frequency;
+
+        let r_height = noise::noise(nx, ny, config.seed)?;
+        let g_height = noise::noise(nx, ny, config.seed + 1)?;
+        let b_height = noise::noise(nx, ny, config.seed + 2)?;
+
+        let r_height = map_noise_to_u8(r_height);
+        let g_height = map_noise_to_u8(g_height);
+        let b_height = map_noise_to_u8(b_height);
+
+        *pixel = Rgb([r_height, g_height, b_height]);
+    }
+    img.save("output.png").unwrap();
+    println!("{max_val}");
+    println!("{min_val}");
 
     Ok(())
 }
 
 fn parse_arguments(args: Vec<String>) -> Result<Config, String> {
-    if args.len() < 4 {
+    if args.len() < 5 {
         return Err("Not enough arguments".into());
     }
 
     let width = args[1].parse().map_err(|_| "Bad width")?;
     let height = args[2].parse().map_err(|_| "Bad height")?;
     let frequency = args[3].parse().map_err(|_| "Bad frequency")?;
-
-    for i in 0..5 {
-        for j in 0..5 {
-            let val = noise::grad_dir_8(i, j, 0);
-            println!("{} {}", val.0, val.1);
-        }
-    }
+    let seed = args[4].parse().map_err(|_| "Bad seed")?;
 
     Ok(Config {
         width: width,
         height: height,
         frequency: frequency,
+        seed: seed,
     })
+}
+fn map_noise_to_u8(x: f64) -> u8 {
+    let clamped = x.clamp(-1.0, 1.0);
+    let normalized = (clamped + 1.0) * 0.5;
+    (normalized * 255.0).round() as u8
 }
 struct Config {
     width: usize,
     height: usize,
     frequency: f64,
+    seed: u32,
 }
--- src/noise.rs
+++ src/noise.rs
@@ -1,8 +1,67 @@
-pub fn noise(x: f64, y: f64) -> Result<f64, String> {
-    Ok(5.0)
+// Public noise function
+pub fn noise(x: f64, y: f64, seed: u32) -> Result<f64, String> {
+    let a = (x.floor(), y.floor());
+    let b = (x.ceil(), y.floor());
+    let c = (x.floor(), y.ceil());
+    let d = (x.ceil(), y.ceil());
+
+    // Because I am doing copies as i32, the tuple values aren't actually moved
+    let va = grad_dir_8(a.0 as i32, a.1 as i32, seed);
+    let vb = grad_dir_8(b.0 as i32, b.1 as i32, seed);
+    let vc = grad_dir_8(c.0 as i32, c.1 as i32, seed);
+    let vd = grad_dir_8(d.0 as i32, d.1 as i32, seed);
+
+    let da = distance(x, y, a.0, a.1);
+    let db = distance(x, y, b.0, b.1);
+    let dc = distance(x, y, c.0, c.1);
+    let dd = distance(x, y, d.0, d.1);
+
+    // Influences
+    let ia = dot_product(da, va);
+    let ib = dot_product(db, vb);
+    let ic = dot_product(dc, vc);
+    let id = dot_product(dd, vd);
+
+    let (u, v) = fade_coordinates((x, y));
+
+    let x_lerp1 = lerp(ia, ib, u);
+    let x_lerp2 = lerp(ic, id, u);
+    let y_lerp = lerp(x_lerp1, x_lerp2, v);
+    Ok(y_lerp)
 }
 
-const GRAD8: [(f32, f32); 8] = [
+fn lerp(a: f64, b: f64, t: f64) -> f64 {
+    a + t * (b - a)
+}
+// Fades coordinate for lerping
+fn fade_coordinates(point: (f64, f64)) -> (f64, f64) {
+    fn fade_scaler(a: f64) -> f64 {
+        (6.0 * a.powf(5.0)) - (15.0 * a.powf(4.0)) + (10.0 * a.powf(3.0))
+    }
+
+    let u = fade_scaler(point.0 - point.0.floor());
+    let v = fade_scaler(point.1 - point.1.floor());
+    (u, v)
+}
+
+// Helper function to compute dot product
+fn dot_product(dist: (f64, f64), vpoint: (f64, f64)) -> f64 {
+    dist.0 * vpoint.0 + dist.1 * vpoint.1
+}
+
+// Helper function to compute distance between points
+fn distance(x1: f64, y1: f64, x2: f64, y2: f64) -> (f64, f64) {
+    let x_diff = x1 - x2;
+    let y_diff = y1 - y2;
+    (x_diff, y_diff)
+}
+
+// given a vertex meet, return a gradient
+fn grad_dir_8(i: i32, j: i32, seed: u32) -> (f64, f64) {
+    GRAD8[grad_index_8(i, j, seed) as usize]
+}
+
+const GRAD8: [(f64, f64); 8] = [
     (1.0, 0.0),       // E
     (0.707, 0.707),   // NE
     (0.0, 1.0),       // N
@@ -27,9 +86,4 @@ fn grad_index_8(i: i32, j: i32, seed: u32) -> u8 {
     let ju = j as u32;
     let h = mix32(iu.wrapping_mul(0x9E37_79B9) ^ ju.wrapping_mul(0x85EB_CA6B) ^ seed);
     (h & 7) as u8 // 0..7
-}
-
-// given a vertex meet, return a gradient
-pub fn grad_dir_8(i: i32, j: i32, seed: u32) -> (f32, f32) {
-    GRAD8[grad_index_8(i, j, seed) as usize]
 }
